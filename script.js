04a3274fb969d719fb3bd6943a500cace0bc8702
David Iscoe <discoe@gmail.com>
2017-08-08 18:00:31 -0400

    buttons in place not yet functional

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var pixels = 16;
+var pixelsize = String(640 / pixels) + "px";
+
 $(document).ready(function(){
+		
 	for (var i=0; i< pixels * pixels; i++) {
-	$(".inner-container").append("<div class='pixel'>" + i + "</div>");
+	$(".inner-container").append("<div class='pixel'></div>");
    }
+   $(".pixel").css({"height": pixelsize});
+   $(".pixel").css({"width": pixelsize});
    $(".pixel").hover(
    	function(){
    		$(this).addClass("etched");
